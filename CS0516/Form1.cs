0efdacd79218f31ad687e3d29b41751af471fcb5
kurebayashi rento <kurebayashi.rat@gmail.com>
2025-05-16 14:31:32 +0900

    u

--- CS0516/Form1.cs
+++ CS0516/Form1.cs
@@ -6,5 +6,17 @@ namespace CS0516
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("‚±‚ñ‚É‚¿‚Í");
+            MessageBox.Show(100.ToString());
+            MessageBox.Show($"{100}");
+            MessageBox.Show($"label1.Top={label1.Top}");
+            MessageBox.Show($"{3.14}");
+
+            MessageBox.Show(1 + 1.ToString());//11
+            MessageBox.Show((1 + 1).ToString());
+        }
     }
 }
